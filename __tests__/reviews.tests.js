3644908e37ba904a91cac396c9bae1e31459a2a1
janelle mellor <janellemellor@gmail.com>
2020-03-27 15:54:10 -0700

    add failing post test for reviews

--- __tests__/reviews.tests.js
+++ __tests__/reviews.tests.js
@@ -1,25 +1,28 @@
-const { getReview, getReviewer } = require('../db/data-helpers');
+const { getReview, getReviewer, getReviews, getFilm } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
 
 describe('reviews routes', () => {
   it('creates a review', async() => {
+    const film = await getFilm();
+    const reviewer = await getReviewer();
+
     return request(app)
       .post('/api/v1/reviews')
       .send({
         rating: 3,
-        reviewer: 'Roger Ebert',
+        reviewer: reviewer._id,
         review: 'This movie was the best!',
-        film: 'Best.Movie.Ever' 
+        film: film._id 
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           rating: 3,
-          reviewer: 'Roger Ebert',
+          reviewer: reviewer._id,
           review: 'This movie was the best!',
-          film: 'Best.Movie.Ever', 
+          film: film._id, 
           __v: 0
         });
       });
