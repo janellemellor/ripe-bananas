a1f1565440b874b9c357478c6473056ee4650c5e
janelle mellor <janellemellor@gmail.com>
2020-03-27 13:40:04 -0700

    add find by id test for film

--- __tests__/films.tests.js
+++ __tests__/films.tests.js
@@ -50,10 +50,18 @@ describe('films routes', () => {
             }
           });
         });   
-       
       });
   });
 
+  it('finds a film by id', async() => {
+    const film = await getFilm();
+    
+    return request(app)
+      .get(`/api/v1/films/${film._id}`)
+      .then(res => {
+        expect(res.body).toEqual(film);
+      });
+  });
 
 });
     
