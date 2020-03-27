07cff45b5574419a901b942d76228e1d26204595
janelle mellor <janellemellor@gmail.com>
2020-03-26 17:35:12 -0700

    add failing get actor test

---
+++ __tests__/actors.tests.js
@@ -0,0 +1,27 @@
+const { getActor, getActors } = require('../db/data-helpers');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('actors routes', () => {
+  it('creates an actor', async() => {
+    return request(app)
+      .post('/api/v1/actors')
+      .send({
+        name: 'Panda Bear',
+        dob: new Date, 
+        pob: 'Canada'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Panda Bear',
+          dob: new Date, 
+          pob: 'Canada',
+          __v: 0
+        });
+      });
+  });
+
+  
+});
